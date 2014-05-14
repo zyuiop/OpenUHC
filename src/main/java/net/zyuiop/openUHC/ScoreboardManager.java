468fb1dbdc8d57eb99268467d86f51f97b914a96
Gnomino <gnomino@gGentooAmd64.gnet>
2014-05-14 12:23:00 +0200

    What if no one wins ?

--- src/main/java/net/zyuiop/openUHC/ScoreboardManager.java
+++ src/main/java/net/zyuiop/openUHC/ScoreboardManager.java
@@ -60,10 +60,14 @@ public class ScoreboardManager implements org.bukkit.scoreboard.ScoreboardManage
 		{
 			Bukkit.getServer().broadcastMessage(ChatColor.GOLD+"L'équipe "+plugin.teams.getTeamsList().get(0).getColorizedName()+ChatColor.GOLD+" a gagné la partie !");
 			plugin.game.finish(plugin.teams.getTeamsList().get(0).getName());
-		} else if (plugin.getGame().solo == true && plugin.joueurs.size() <= 1) {
-			if (!plugin.game.isWon) {
+		} else if (plugin.getGame().solo == true && plugin.joueurs.size() == 1) {
+			if (!plugin.getGame().isWon) {
 				Bukkit.getServer().broadcastMessage(ChatColor.GOLD+"Le joueur "+plugin.joueurs.get(0).getDisplayName() +" a gagné la partie !");
 				plugin.game.finish(plugin.joueurs.get(0).getDisplayName());
+			}
+			else if (plugin.getGame().solo && plugin.joueurs.size() == 0) {
+				Bukkit.getServer().broadcastMessage(ChatColor.GOLD + "Égalité, personne ne gagne !");
+				plugin.getGame().finish("");
 			}
 		}
 	}
